0556df07f882d9e5306c20f2e3a4f5cea34cdff1
Kishore <kishorereddykalluru@gmail.com>
2022-05-26 22:44:39 +0530

    Algoexpert

--- src/algoexpert/strings/easy/FirstNonRepeatingCharacter.java
+++ src/algoexpert/strings/easy/FirstNonRepeatingCharacter.java
@@ -7,20 +7,21 @@ public class FirstNonRepeatingCharacter {
 
     public static int firstNonRepeatingCharacter(String string) {
         Map<Character, Integer> frequencyMap = new HashMap<>();
+
         for(int i = 0; i < string.length(); i++) {
-            frequencyMap.put(string.charAt(i), frequencyMap.getOrDefault(string.charAt(i), 0) + 1);
+        frequencyMap.put(string.charAt(i), frequencyMap.getOrDefault(string.charAt(i), 0) + 1);
         }
 
         for(int i = 0; i < string.length(); i++){
-            if(frequencyMap.get(string.charAt(i)) == 1)
-                return i;
+        if(frequencyMap.get(string.charAt(i)) == 1)
+        return i;
         }
 
         return -1;
-    }
+        }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         String str = "abcdcaf";
         System.out.println(firstNonRepeatingCharacter(str));
-    }
-}
+        }
+        }
